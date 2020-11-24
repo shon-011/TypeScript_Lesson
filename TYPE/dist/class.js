4ecd0d9b812d87d131a2591d2a160601e6dced42
shona <f.mb@outlook.jp>
2020-11-24 09:15:55 +0900

    object生成

---
+++ TYPE/dist/class.js
@@ -0,0 +1,8 @@
+"use strict";
+class Person {
+    constructor(initName) {
+        this.name = initName;
+    }
+}
+const shon = new Person('shona');
+console.log(shon);
---
+++ TYPE/src/class.ts
@@ -0,0 +1,9 @@
+class Person {
+    name: string;
+    constructor(initName: string){
+        this.name = initName;
+    }
+}
+
+const shon = new Person('shona');
+console.log(shon);
