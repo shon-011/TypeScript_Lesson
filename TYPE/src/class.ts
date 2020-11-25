860b4cfb8d7bba1b52b1bbc6da112424f20428b0
shona <f.mb@outlook.jp>
2020-11-25 23:55:17 +0900

    class

--- TYPE/src/class.ts
+++ TYPE/src/class.ts
@@ -23,3 +23,18 @@ const shon = new Person('shona',22);
 shon.incrementAge();
 // shon.age; priveteはclass外の読み取りもできない
 shon.say();
+
+
+
+//初期化省略
+// class Person {
+//     constructor(public name: string, private age: number){
+
+//     }
+// }
+
+class sports {
+    constructor(public name: string, ){
+
+    }
+}
